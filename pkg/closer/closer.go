fa00083a7408af7edc2f3292fcf10ddadc8830c4
vadvoronin@gmail.com <greendarkroom90>
2024-08-15 19:55:12 +0300

    ReadCommitted + sigs for closer added

--- pkg/closer/closer.go
+++ pkg/closer/closer.go
@@ -5,9 +5,10 @@ import (
 	"os"
 	"os/signal"
 	"sync"
+	"syscall"
 )
 
-var globalCloser = New()
+var globalCloser = New(syscall.SIGTERM, syscall.SIGINT)
 
 // Add adds `func() error` callback to the globalCloser
 func Add(f ...func() error) {
